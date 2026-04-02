2132910e9402e67abe01c564d95e179eaedf6457
putraadriyan544-collab <putraadriyan544@gmail.com>
2026-04-02 22:02:05 +0700

    250441100029-ADRIYAN RUNASSYIYAM PUTRA-AVRIZAL RENDI PRAYOGA-MODUL 1

---
+++ modul-1/250441100029_ADRIYAN RUNASSYIYAM PUTRA/soal 2.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE perusahaan;
+use perusahaan;
+CREATE TABLE pelanggan_setia(
+	nomor_identitas INT
+);
+SHOW TABLES;
+DESCRIBE pelanggan_setia;
+
+ALTER TABLE pelanggan_setia
+MODIFY COLUMN nomor_identitas VARCHAR(20);
+
